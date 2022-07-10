36500c790e0536f41a3f9d40ca5b09103dc9a375
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-10 21:14:24 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-st.title('Sanjana\'s Dessert Corner')
+st.title('Sanjana\'s Dessert Corner:ice_cream:')
 
 st.header('Today\'s Dessert Menu')
 
-st.text('Vanilla Ice Cream \t\t\t\t Rs. 20')
-st.text('Shaved Ice \t\t\t\t Rs. 25')
-st.text('Strawberry Cake \t\t\t\t Rs. 55')
-st.text('Chocolate Cake \t\t\t\t Rs. 65')
-st.text('Choco-chip Cookies \t\t\t\t Rs. 30')
-st.text('Choco glazed Doughnut \t\t\t\t Rs. 20')
-st.text('Chocolate Cupcake with Strawberry frosting \t\t\t\t Rs. 35')
-st.text('Apple Pie \t\t\t\t Rs. 35')
+st.text(':icecream:Vanilla Ice Cream \t\t\t\t Rs. 20')
+st.text(':shaved_ice:Shaved Ice \t\t\t\t Rs. 25')
+st.text(':cake:Strawberry Cake \t\t\t\t Rs. 55')
+st.text(':birthday:Chocolate Cake \t\t\t\t Rs. 65')
+st.text(':cookie:Choco-chip Cookies \t\t\t\t Rs. 30')
+st.text(':doughnut:Choco glazed Doughnut \t\t\t\t Rs. 20')
+st.text(':cupcake:Chocolate Cupcake with Strawberry frosting \t\t\t\t Rs. 35')
+st.text(':pie:Apple Pie \t\t\t\t Rs. 35')
