45ebd3d4e6717857ec3d8d8b256d685de560851f
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-10 22:44:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('Sanjana\'s Dessert Corner:ice_cream:')
 
+#Dessert
 st.header('Today\'s Dessert Menu')
 
 st.text('🍦Vanilla Ice Cream\t\t\t\t\tRs. 20')
@@ -12,3 +13,8 @@ st.text('🍪Choco-chip Cookies\t\t\t\t\tRs. 30')
 st.text('🍩Choco glazed Doughnut\t\t\t\t\tRs. 20')
 st.text('🧁Chocolate Cupcake with Strawberry frosting\t\tRs. 35')
 st.text('🥧Apple Pie\t\t\t\t\t\tRs. 35')
+
+#Fruit
+import pandas as pd
+fruit_df_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(fruit_df_list)
