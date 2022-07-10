1ed3e7bb5714bbaa63a0318ace1ca3eaa1db0c57
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-10 21:11:08 +0530

    Update streamlit_app.py
    Changed the page from breakfast to dessert

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-st.title('Sanjana\'s Diner')
+st.title('Sanjana\'s Dessert Corner')
 
-st.header('Today\'s Breakfast Menu')
+st.header('Today\'s Dessert Menu')
 
-st.text('Idly \t\t Rs. 20')
-st.text('Dosa \t\t Rs. 30')
-st.text('Vada \t\t Rs. 25')
-st.text('Upma \t\t Rs. 25')
-st.text('Poori \t\t Rs. 35')
+st.text('Vanilla Ice Cream \t\t\t\t Rs. 20')
+st.text('Shaved Ice \t\t\t\t Rs. 25')
+st.text('Strawberry Cake \t\t\t\t Rs. 55')
+st.text('Chocolate Cake \t\t\t\t Rs. 65')
+st.text('Choco-chip Cookies \t\t\t\t Rs. 30')
+st.text('Choco glazed Doughnut \t\t\t\t Rs. 20')
+st.text('Chocolate Cupcake with Strawberry frosting \t\t\t\t Rs. 35')
+st.text('Apple Pie \t\t\t\t Rs. 35')
