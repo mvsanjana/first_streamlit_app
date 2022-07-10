fa2605db3c244d00e13dd6e32afe97d4598527a8
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-10 20:59:22 +0530

    Update streamlit_app.py
    Added Breakfast menu items using header and text functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
+
 st.title('Sanjana\'s Diner')
+
+st.header('Today\'s Breakfast Menu')
+
+st.text('Idly \t\t Rs. 20')
+st.text('Dosa \t\t Rs. 30')
+st.text('Vada \t\t Rs. 25')
+st.text('Upma \t\t Rs. 25')
+st.text('Poori \t\t Rs. 35')
