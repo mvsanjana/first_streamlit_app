e8eefd6bc991e783fff0b99733e009e52a48e795
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-11 11:13:49 +0530

    Update streamlit_app.py
    Converted options in multi-select from numbers to fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,6 @@ st.header('Build your own Fruit Smoothie🍎')
 fruit_df_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
+fruit_df_list=fruit_df_list.setIndex('Fruit')
 st.multiselect("Pick some fruits: ",list(fruit_df_list.index))
 st.dataframe(fruit_df_list)
