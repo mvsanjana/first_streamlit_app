386b3cafd560698c57982b3179208be079a86afa
M V SANJANA <66083273+mvsanjana@users.noreply.github.com>
2022-07-27 22:05:39 +0530

    Update streamlit_app.py
    Added code to query snowflake account metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,11 @@ fruityvice_response = rqs.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #Output as a table on the screen
 st.dataframe(fruityvice_normalized)
+
+#Test connection to Snowflake from Streamlit
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetch()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
